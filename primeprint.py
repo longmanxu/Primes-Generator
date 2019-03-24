61751d51d76b9ed7df21000b690872e733bde2ae
Longman Xu <longmanxu@live.ca>
2019-03-23 21:04:32 -0400

    added printing primes out function

---
+++ primeprint.py
@@ -0,0 +1,17 @@
+import primelookupgen
+import time
+
+n = int(input("What's the highest number (exclusive) you want to check?\n"))
+
+start_time = time.time()
+
+A = primelookupgen.gen_lookup_table(n)
+for i in range(n):
+    if A[i]:
+        print(i)
+
+print("Run time: ", end="")
+print(time.time() - start_time, end="")
+print("s")
+
+input("Press enter to close...\n")
