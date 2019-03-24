99419ef6d6308571aba9e708670e43d0bfdebb9b
Longman Xu <longmanxu@live.ca>
2019-03-23 20:32:24 -0400

    function to create prime lookup table

---
+++ primelookupgen.py
@@ -0,0 +1,23 @@
+import math
+
+
+def primegen(n):
+    """
+    Generates and returns a lookup table of primes up to n
+
+    :param int n: the length of the lookup table
+    :return: the lookup table
+    """
+
+    A = [True for _ in range(n)]
+
+    A[0] = A[1] = False
+
+    for i in range(2, math.ceil(math.sqrt(n))):
+        if not A[i]:
+            continue
+        else:
+            for j in range(i*i, n, i):
+                A[j] = False
+
+    return A
